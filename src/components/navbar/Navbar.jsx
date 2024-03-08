db0f897437eef560f7688b5a0349d30e45fa9d0c
Amit Kumar <aka883207@gmail.com>
2024-03-08 11:31:12 +0600

    basic react-setup complete

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">Advisoropedia</a>
+            </div>
+            
+            <div className="navbar-end">
+                <a className="btn">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
